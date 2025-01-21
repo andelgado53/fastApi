6332e1a6719e3b7d55ab619de66e325d4b2fe2af
Andres Delgado <delandre@amazon.com>
2025-01-21 13:53:13 -0800

    Update github username

--- fast_api_prototype/pipeline_stack.py
+++ fast_api_prototype/pipeline_stack.py
@@ -11,7 +11,7 @@ class PipelineStack(Stack):
 
         # Define GitHub source connection
         source = pipelines.CodePipelineSource.connection(
-            "delandre/fastApi",  # Replace with your repo
+            "andelgado53/fastApi",  # Replace with your repo
             "master",
             connection_arn="arn:aws:codeconnections:us-west-2:385249579775:connection/acd4b90d-7dfa-4f19-aad5-b68d9b524840"
         )
