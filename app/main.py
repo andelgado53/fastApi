3f6d8c5f2aae09058c2d7ad7b53242a235212602
Andres Delgado <delandre@amazon.com>
2025-01-23 18:05:29 -0800

    Update apid response

--- app/main.py
+++ app/main.py
@@ -1,42 +1,12 @@
 from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
 
-@app.get("/", response_class= HTMLResponse)
+@app.get("/")
 def root():
-	response = """
-	<!DOCTYPE html>
-	<html lang="en">
-	<head>
-  	<meta charset="UTF-8">
-  	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-  	<title>Emi Sofia</title>
-  	<style>
-    body {
-      font-family: Arial, sans-serif;
-      text-align: center;
-      margin: 20px;
-    }
-    img {
-      max-width: 60%;
-      height: 10%;
-      border: 2px solid #ccc;
-      box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.1);
-    }
-  	</style>
-	</head>
-	<body>
-	<h1>Emi is turning 5!!!</h1>
-	<video controls>
-  	<source src="https://emisofia.s3.us-west-2.amazonaws.com/Bluey-Emi-3550868620v2.mp4" type="video/mp4">
-	</video>
-	</body>
-	</html>
-	"""
-	return response
+	return { "message": "Emi is loved."}
 
 @app.get("/healthy")
 def health_check():
-	return "healthy"
+	return {"message": "healthy"}
